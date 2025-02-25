32b2bfebad8807f9c13b6743ede0524e46127788
Massimiliano Galli <mg2708@della-vis1.princeton.edu>
2025-02-25 03:53:25 -0500

    latest updates on flows

--- cms_ttbar_open_data/try_evaluate_flows.py
+++ cms_ttbar_open_data/try_evaluate_flows.py
@@ -7,6 +7,7 @@ import pickle as pkl
 
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     processes = ["ttbar", "wjets", "single_top_t_chan"]
     #processes = ["wjets"]
 
@@ -24,13 +25,22 @@ if __name__ == '__main__':
             dataset[column] = scaler.fit_transform(dataset[[column]]).flatten()
 
         # load the trained flow
-        flow = zuko.flows.NSF(features=len(columns), context=0, transforms=4)
+        #flow = zuko.flows.NSF(features=len(columns), context=0, transforms=4)
+        flow = zuko.flows.NSF(features=len(columns), context=0, transforms=3)
         flow.load_state_dict(torch.load(f"flows/flow_{process}.pt"))
+        flow = flow.to(device)
 
-        # compute prob
-        log_prob = flow().log_prob(torch.tensor(dataset[columns].values).float())
-        prob = torch.exp(log_prob).detach().numpy()
-        print(prob)
+        # compute prob in batches to avoid memory issues
+        bs = 1024
+        log_prob_list = []
+        tot_batches = len(dataset) // bs
+        for i in range(0, len(dataset), bs):
+            print(f"Batch {i//bs}/{tot_batches}")
+            data = torch.tensor(dataset[columns].values[i:i+bs]).float().to(device)
+            log_prob = flow().log_prob(data)
+            prob = torch.exp(log_prob).detach().cpu().numpy()
+            log_prob_list.append(prob)
+        prob = np.concatenate(log_prob_list)
         probs[process] = prob
     
     out_path = "flows/probs.pkl"
--- cms_ttbar_open_data/try_flows.py
+++ cms_ttbar_open_data/try_flows.py
@@ -104,88 +104,89 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device: {}".format(device))
 
-    saved_data='./cached_data/'
-    dataset = pd.read_hdf(saved_data + "dataset_preselected_ttbar.h5", "dataset")
-    print(dataset.head())
-    print(dataset.columns)
-    print(len(dataset))
+    #processes = ["ttbar", "wjets", "single_top_t_chan"]
+    processes = ["ttbar"]
+    #processes = ["wjets", "single_top_t_chan"]
+    for process in processes:
+        #process = "wjets"
+        #process = "ttbar"
+        #process = "single_top_t_chan"
 
-    #process = "wjets"
-    process = "ttbar"
-    #process = "single_top_t_chan"
+        saved_data='./cached_data/'
+        dataset = pd.read_hdf(saved_data + "dataset_preselected_ttbar.h5", "dataset")
 
-    dataset = dataset[dataset["type"] == process]
-    print(len(dataset))
+        dataset = dataset[dataset["type"] == process]
+        print(len(dataset))
 
-    # keep only 30% of events if process is ttbar
-    if process == "ttbar":
-        dataset = dataset.sample(frac=0.3, random_state=42)
+        # keep only 30% of events if process is ttbar
+        if process == "ttbar":
+            dataset = dataset.sample(frac=0.3, random_state=42)
 
-    columns = ['log_lepton_pt', 'log_H_T', 'lepton_eta', 'lepton_phi']
+        columns = ['log_lepton_pt', 'log_H_T', 'lepton_eta', 'lepton_phi']
 
-    # scale to (-5, 5) where the spline is defined
-    for column in columns:
-        scaler = MinMaxScaler(feature_range=(-5, 5))
-        dataset[column] = scaler.fit_transform(dataset[[column]]).flatten()
+        # scale to (-5, 5) where the spline is defined
+        for column in columns:
+            scaler = MinMaxScaler(feature_range=(-5, 5))
+            dataset[column] = scaler.fit_transform(dataset[[column]]).flatten()
 
-    #flow = zuko.flows.MAF(features=len(columns), context=0, transforms=4)
-    flow = zuko.flows.NSF(features=len(columns), context=0, transforms=4)
-    flow.to(device)
+        flow = zuko.flows.NSF(features=len(columns), context=0, transforms=3)
+        flow.to(device)
 
-    epochs = 50
-    early_stopper = EarlyStopper(patience = 15, min_delta=0.000)
-    batch_size = 256
-    learning_rate = 1e-3
-    optimizer = torch.optim.Adam(flow.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
+        epochs = 400
+        #early_stopper = EarlyStopper(patience = 15, min_delta=0.000)
+        batch_size = 256
+        learning_rate = 1e-3
+        optimizer = torch.optim.Adam(flow.parameters(), lr=learning_rate)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
 
-    train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
-    train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.2, random_state=42)
-    train_dataset = CustomDataset(train_dataset, columns, device)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dataset = CustomDataset(val_dataset, columns, device)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_dataset = CustomDataset(test_dataset, columns, device)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+        train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+        train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.2, random_state=42)
+        train_dataset = CustomDataset(train_dataset, columns, device)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_dataset = CustomDataset(val_dataset, columns, device)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        test_dataset = CustomDataset(test_dataset, columns, device)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    train_history = []
-    test_history = []
-    for epoch in range(epochs):
-        start = time.time()
-        print(f"Epoch {epoch}/{epochs}")
-        print("Training...")
-        train_losses, test_losses = [], []
-        for i, (data, weights) in enumerate(train_loader):
-            flow.train()
-            optimizer.zero_grad()
-            loss = -flow().log_prob(data)# * weights
-            loss = loss.mean()
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-            train_losses.append(loss.item())
-        train_history.append(np.mean(train_losses))
-
-        print("Validating...")
-        for i, (data, weights) in enumerate(val_loader):
-            with torch.no_grad():
-                flow.eval()
+        train_history = []
+        test_history = []
+        for epoch in range(epochs):
+            start = time.time()
+            print(f"Epoch {epoch}/{epochs}")
+            print("Training...")
+            train_losses, test_losses = [], []
+            for i, (data, weights) in enumerate(train_loader):
+                flow.train()
+                optimizer.zero_grad()
                 loss = -flow().log_prob(data)# * weights
                 loss = loss.mean()
-                test_losses.append(loss.item())
-        test_history.append(np.mean(test_losses))
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                train_losses.append(loss.item())
+            train_history.append(np.mean(train_losses))
 
-        duration = time.time() - start
-        print(
-            f"Epoch {epoch} | Rank {device} - train loss: {train_history[epoch]:.4f} - val loss: {test_history[epoch]:.4f} - time: {duration:.2f}s"
-        )
-        if early_stopper.early_stop(test_history[epoch]) or epoch == epochs - 1:
-            print("Early stopping")
-            # save the model
-            # make dir if does not exist
-            if not os.path.exists("flows/"):
-                os.makedirs("flows/") 
-            torch.save(flow.state_dict(), f"flows/flow_{process}.pt")
-            plot_loss(train_history, test_history, "flows/", process)
-            evaluate_flow(flow, test_loader, "flows/", columns, process)
-            break
+            print("Validating...")
+            for i, (data, weights) in enumerate(val_loader):
+                with torch.no_grad():
+                    flow.eval()
+                    loss = -flow().log_prob(data)# * weights
+                    loss = loss.mean()
+                    test_losses.append(loss.item())
+            test_history.append(np.mean(test_losses))
+
+            duration = time.time() - start
+            print(
+                f"Epoch {epoch} | Rank {device} - train loss: {train_history[epoch]:.4f} - val loss: {test_history[epoch]:.4f} - time: {duration:.2f}s"
+            )
+            #if early_stopper.early_stop(test_history[epoch]) or epoch == epochs - 1:
+            if epoch == epochs - 1:
+                print("Early stopping")
+                # save the model
+                # make dir if does not exist
+                if not os.path.exists("flows/"):
+                    os.makedirs("flows/") 
+                torch.save(flow.state_dict(), f"flows/flow_{process}.pt")
+                plot_loss(train_history, test_history, "flows/", process)
+                evaluate_flow(flow, test_loader, "flows/", columns, process)
+                break
